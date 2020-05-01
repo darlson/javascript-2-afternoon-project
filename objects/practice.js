0dc3bc3ac72d0f29350fccd496a1306763053f2b
darlson <davertiser@gmail.com>
2020-04-30 22:14:57 -0600

    updated arrays practice

--- objects/practice.js
+++ objects/practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+const first = (arr) => {
+  return arr[0]
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+const last = (arr) => {
+  return arr[arr.length-1]
+}
 
 
 
@@ -47,8 +51,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+const looper = (family) => {
+  for (let i = 0; i < family.length; i++ )
+  alert(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,7 +68,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+const reversedLooper = (letters) => {
+  for (let i = letters.length - 1; i>= 0; i-- )
+  alert(letters[i])
+}
 
 
 
@@ -77,9 +86,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
-
-
+const evenFinder = (nums) => {
+  for (let i = nums.length - 1; i >= 0; i--)
+  if (nums[i] % 2 != 0) {
+    console.log(nums)
+    console.log(nums[i])
+    nums.splice(i,1)
+  }
+  return nums
+}
 
 
 
@@ -106,7 +121,9 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
   and the second item in the array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+const divider = (numbersArray) => {
+  return 
+}
 
 
 
